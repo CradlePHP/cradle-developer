2823689f7582f1b07cbf965e4d171d1230e4df3c
Charles Zamora <charles.andacc@gmail.com>
2018-03-15 14:55:37 +0800

    put the placeholders to suppress errors

---
+++ src/events/generate/module.php
@@ -0,0 +1,20 @@
+<?php //-->
+/**
+ * This file is part of the Cradle PHP Kitchen Sink Faucet Project.
+ * (c) 2016-2018 Openovate Labs
+ *
+ * Copyright and license information can be found at LICENSE.txt
+ * distributed with this package.
+ */
+
+/**
+ * CLI help menu
+ *
+ * @param Request $request
+ * @param Response $response
+ *
+ * @return string
+ */
+return function ($request, $response) {
+    
+};
