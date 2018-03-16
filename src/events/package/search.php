56c3b3de078eb35b71e97958341f418223a3fafa
Charles Zamora <charles.andacc@gmail.com>
2018-03-16 18:03:31 +0800

    package search via packagist

--- src/events/package/search.php
+++ src/events/package/search.php
@@ -1,4 +1,14 @@
 <?php //-->
+/**
+ * This file is part of the Cradle PHP Library.
+ * (c) 2016-2018 Openovate Labs
+ *
+ * Copyright and license information can be found at LICENSE.txt
+ * distributed with this package.
+ */
+
+use Cradle\Framework\CommandLine;
+use Cradle\Composer\Packagist;
 
 /**
  * $ cradle package search foo/bar
@@ -7,6 +17,65 @@
  * @param Response $response
  */
 return function($request, $response) {
-    $request->setStage('q', $request->getStage(0));
-    $this->trigger('system-package-search', $request, $response);
+    CommandLine::info('Searching for packages...');
+
+    // initialize packagist
+    $packagist = new Packagist();
+
+    // query set?
+    if ($request->hasStage('q')) {
+        // set query
+        $packagist->setQuery($request->getStage('q'));
+    }
+
+    // type set?
+    if ($request->hasStage('type')) {
+        // set type
+        $packagist->setType($request->getStage('type'));
+    } else {
+        // set default type
+        $packagist->setType('cradle-package');
+    }
+
+    // page set?
+    if ($request->hasStage('page')) {
+        // set page
+        $packagist->setPage($request->getStage('page'));
+    } else {
+        // set default page
+        $packagist->setPage(1);
+    }
+
+    // range set?
+    if ($request->hasStage('range')) {
+        // set range
+        $packagist->setPerPage($request->getStage('range'));
+    } else {
+        // set default range
+        $packagist->setPerPage(10);
+    }
+
+    // get the packages
+    $packages = $packagist->search();
+
+    // if we have packages
+    if (isset($packages['results'])
+    && !empty($packages['results'])) {
+        CommandLine::info(sprintf('Found %s package(s)...', $packages['total']));
+  
+        // print each package
+        foreach($packages['results'] as $package) {
+            CommandLine::success(sprintf(
+                '* %s - %s',
+                $package['name'],
+                $package['description']
+            ));
+        }
+
+        CommandLine::warning('Run: `$ cradle package install vendor/package` to install a package...');
+    
+        return;
+    }
+
+    CommandLine::error('No package(s) found', false);
 };
