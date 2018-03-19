e290e1e84c23f9d720e8752dc811e97f783fdb70
Charles Zamora <charles.andacc@gmail.com>
2018-03-19 12:49:11 +0800

    update masthead

--- src/events/package/update.php
+++ src/events/package/update.php
@@ -1,4 +1,11 @@
 <?php //-->
+/**
+ * This file is part of the Cradle PHP Library.
+ * (c) 2016-2018 Openovate Labs
+ *
+ * Copyright and license information can be found at LICENSE.txt
+ * distributed with this package.
+ */
 
 use Cradle\Framework\CommandLine;
 
